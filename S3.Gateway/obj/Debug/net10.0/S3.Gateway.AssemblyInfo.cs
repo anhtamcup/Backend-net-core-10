22399e0904204ddc3c845a6334908dad0386d9ad
anhtamcup <tamla@cfox.vn>
2026-04-23 17:40:48 +0700

    Updated

--- S3.Gateway/obj/Debug/net10.0/S3.Gateway.AssemblyInfo.cs
+++ S3.Gateway/obj/Debug/net10.0/S3.Gateway.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("S3.Gateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7db0e47f7a1a909fd054f21c7a68582a430b05f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99991999a8d425d489c5a7b495d676186f3e15fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("S3.Gateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("S3.Gateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
