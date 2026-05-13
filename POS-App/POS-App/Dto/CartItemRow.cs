6a23a56bf8562c00b034bab5a6a4df674db71621
anhtamcup <tamla@cfox.vn>
2026-05-13 17:43:04 +0700

    Updated

---
+++ POS-App/POS-App/Dto/CartItemRow.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POS_App.Dto
+{
+    public class CartItemRow
+    {
+        public int ID { get; set; }
+        public int Index { get; set; }
+        public string Code { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public decimal OriginalPrice { get; set; }
+        public decimal DiscountPrice { get; set; }
+        public int Quantity { get; set; }
+        public decimal TotalPrice { get; set; }
+        public string Note { get; set; } = string.Empty;
+        public string Unit { get; set; } = string.Empty;
+        public bool IsSelected { get; set; }
+    }
+}
--- POS-App/POS-App/Views/OrderView.xaml.cs
+++ POS-App/POS-App/Views/OrderView.xaml.cs
@@ -1,4 +1,5 @@
 ﻿
+using POS_App.Dto;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -14,83 +15,36 @@ namespace POS_App.Views
         public OrderView()
         {
             InitializeComponent();
-            //CreateKeyboard();
-        }
 
-        private void KeyboardButton_Click(object sender, RoutedEventArgs e)
-        {
-            var btn = sender as Button;
-            string value = btn.Content.ToString();
 
-            MessageBox.Show(value);
-        }
-
-        private void AddButton(Grid grid, string text, int row, int col, string backgroundColor = "", bool isEnter = false)
-        {
-            Button btn = new Button
+            var cartItems = new List<CartItemRow>();
+            cartItems.Add(new CartItemRow
             {
-                Height=54,
-                Width=109,
-                Content = text,
-                Margin = new Thickness(1),
-                FontSize = 18,
-                BorderThickness = new Thickness(0)
-            };
+                ID = 0,
+                Index = 1,
+                Code = "00000001",
+                Name = "Nước suôits Lavie 500ml",
+                OriginalPrice = 10000,
+                Quantity = 2,
+                Unit = "Chai",
+                DiscountPrice = 0,
+                TotalPrice = 20000
+            });
 
-            btn.Background = (Brush)FindResource(backgroundColor);
-            btn.Foreground = (Brush)FindResource("keyboard-button-text");
+            cartItems.Add(new CartItemRow
+            {
+                ID = 0,
+                Index = 1,
+                Code = "00000002",
+                Name = "Nước suôits Lavie 500ml",
+                OriginalPrice = 100000,
+                Quantity = 2,
+                Unit = "Chai",
+                DiscountPrice = 0,
+                TotalPrice = 200000
+            });
 
-            Grid.SetRow(btn, row);
-            Grid.SetColumn(btn, col);
-            Grid.SetRowSpan(btn, isEnter ? 2 : 1);
-
-            grid.Children.Add(btn);
-        }
-
-        private void CreateKeyboard()
-        {
-            Grid grid = new Grid();
-
-            // rows
-            for (int i = 0; i < 5; i++)
-                grid.RowDefinitions.Add(new RowDefinition());
-
-            // columns
-            for (int i = 0; i < 4; i++)
-                grid.ColumnDefinitions.Add(new ColumnDefinition());
-
-            // Row 0
-            AddButton(grid, "7", 0, 0, backgroundColor: "bg-button-white");
-            AddButton(grid, "8", 0, 1, backgroundColor: "bg-button-white");
-            AddButton(grid, "9", 0, 2, backgroundColor: "bg-button-white");
-            AddButton(grid, "⌫", 0, 3, backgroundColor: "bg-button-gray");
-
-            // Row 1
-            AddButton(grid, "4", 1, 0, backgroundColor: "bg-button-white");
-            AddButton(grid, "5", 1, 1, backgroundColor: "bg-button-white");
-            AddButton(grid, "6", 1, 2, backgroundColor: "bg-button-white");
-            AddButton(grid, "↑", 1, 3, backgroundColor: "bg-button-gray");
-
-            // Row 2
-            AddButton(grid, "1", 2, 0, backgroundColor: "bg-button-white");
-            AddButton(grid, "2", 2, 1, backgroundColor: "bg-button-white");
-            AddButton(grid, "3", 2, 2, backgroundColor: "bg-button-white");
-            AddButton(grid, "↓", 2, 3, backgroundColor: "bg-button-gray");
-
-            // Row 3
-            AddButton(grid, "%", 3, 0, backgroundColor: "bg-button-white");
-            AddButton(grid, ",", 3, 1, backgroundColor: "bg-button-white");
-            AddButton(grid, "0", 3, 2, backgroundColor: "bg-button-white");
-
-            // ENTER span 2 rows
-            AddButton(grid, "ENTER", 3, 3, backgroundColor: "bg-button-green", isEnter: true);
-
-            // Row 4
-            AddButton(grid, "CANCEL", 4, 0, backgroundColor: "bg-button-gray");
-            AddButton(grid, "DEL", 4, 1, backgroundColor: "bg-button-gray");
-            AddButton(grid, "CLEAR", 4, 2, backgroundColor: "bg-button-gray");
-
-            //KeyboardContainer.Children.Add(grid);
+            grCartItems.ItemsSource = cartItems;
         }
     }
 }
