95e1026bf611191929c71542678e19a52a4d637a
anhtamcup <tamla@cfox.vn>
2026-05-14 11:29:43 +0700

    Updated

--- POS-App/POS-App/Views/OrderView.xaml.cs
+++ POS-App/POS-App/Views/OrderView.xaml.cs
@@ -1,50 +1,81 @@
-﻿
-using POS_App.Dto;
+﻿using POS_App.Dto;
+using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Input;
-using System.Windows.Media;
+using System.Windows.Data;
 
 namespace POS_App.Views
 {
-    /// <summary>
-    /// Interaction logic for OrderView.xaml
-    /// </summary>
     public partial class OrderView : UserControl
     {
+        private ObservableCollection<CartItemRow> cartItems;
+        private CollectionViewSource cvs;
+
         public OrderView()
         {
             InitializeComponent();
 
+            cartItems = new ObservableCollection<CartItemRow>();
 
-            var cartItems = new List<CartItemRow>();
-            cartItems.Add(new CartItemRow
+            for (int i = 0; i < 10; i++)
             {
-                ID = 0,
-                Index = 1,
-                Code = "00000001",
-                Name = "Nước suôits Lavie 500ml",
-                OriginalPrice = 10000,
-                Quantity = 2,
-                Unit = "Chai",
-                DiscountPrice = 0,
-                TotalPrice = 20000
-            });
+                cartItems.Add(new CartItemRow
+                {
+                    ID = i,
+                    Index = i + 1,
+                    Code = "0000000" + i,
+                    Name = "Nước suối Lavie 500ml",
+                    OriginalPrice = 10000,
+                    Quantity = 2,
+                    Unit = "Chai",
+                    DiscountPrice = 0
+                });
+            }
 
-            cartItems.Add(new CartItemRow
+            cvs = new CollectionViewSource();
+            cvs.Source = cartItems;
+
+            cvs.SortDescriptions.Add(
+                new System.ComponentModel.SortDescription(
+                    nameof(CartItemRow.Index),
+                    System.ComponentModel.ListSortDirection.Descending));
+
+            grCartItems.ItemsSource = cvs.View;
+        }
+
+        private void btnPlus_Click(object sender, RoutedEventArgs e)
+        {
+            var item = (sender as Button)?.DataContext as CartItemRow;
+
+            if (item == null)
+                return;
+
+            item.Quantity++;
+        }
+
+        private void btnMinus_Click(object sender, RoutedEventArgs e)
+        {
+            var item = (sender as Button)?.DataContext as CartItemRow;
+
+            if (item == null)
+                return;
+
+            item.Quantity--;
+
+            if (item.Quantity <= 0)
             {
-                ID = 0,
-                Index = 1,
-                Code = "00000002",
-                Name = "Nước suôits Lavie 500ml",
-                OriginalPrice = 100000,
-                Quantity = 2,
-                Unit = "Chai",
-                DiscountPrice = 0,
-                TotalPrice = 200000
-            });
+                cartItems.Remove(item);
+            }
+        }
 
-            grCartItems.ItemsSource = cartItems;
+        private void btnRemoveItem_Click(object sender, RoutedEventArgs e)
+        {
+            var item = (sender as Button)?.DataContext as CartItemRow;
+
+            if (item == null)
+                return;
+
+            cartItems.Remove(item);
         }
     }
 }
