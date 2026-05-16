8348104ef8cd376e0f9b69266c0edaaf8a3d11a2
anhtamcup <tamla@cfox.vn>
2026-05-16 17:07:53 +0700

    Updated

--- POS-App/POS-App/MainWindow.xaml.cs
+++ POS-App/POS-App/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
-﻿using POS_App.ViewModels;
+﻿using POS_App.Helpers;
+using POS_App.ViewModels;
+using POS_App.Views;
 using System.Windows;
 using System.Windows.Media;
-
+//using System.Windows.Forms;
 namespace POS_App
 {
     /// <summary>
@@ -14,12 +16,44 @@ namespace POS_App
             RenderOptions.ProcessRenderMode = System.Windows.Interop.RenderMode.SoftwareOnly;
             InitializeComponent();
             DataContext = new MainViewModel();
+
+            Loaded += MainWindow_Loaded;
+            OpenCustomerScreen();
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            WindowStyle = WindowStyle.None;
+            ResizeMode = ResizeMode.NoResize;
+
+            var area = SystemParameters.WorkArea;
+
+            Left = area.Left;
+            Top = area.Top;
+            Width = area.Width;
+            Height = area.Height;
+
+            Topmost = true;
         }
 
         private void btnLog_Click(object sender, RoutedEventArgs e)
         {
             var vm = DataContext as MainViewModel;
             vm?.Logout();
+        }
+
+        private void OpenCustomerScreen()
+        {
+            var rect = MonitorUtil.GetSecondaryScreen(this);
+
+            var w = new CustomerWindow();
+
+            w.WindowStartupLocation = WindowStartupLocation.Manual;
+            w.Left = rect.Left;
+            w.Top = rect.Top;
+            w.WindowState = WindowState.Maximized;
+
+            w.Show();
         }
     }
 }
